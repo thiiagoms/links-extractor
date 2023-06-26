3d13c0917870ec186b3e9d72fbfaf17b7687b4a4
Thiago <54003589+thiiagoms@users.noreply.github.com>
2023-06-25 23:58:43 -0300

    Update test_extractor.py

--- tests/unit/test_extractor.py
+++ tests/unit/test_extractor.py
@@ -2,13 +2,8 @@ import unittest
 import os
 import sys
 
-# Obtenha o diretório atual do arquivo de teste
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Vá três níveis acima para chegar ao diretório desejado (src)
 src_dir = os.path.abspath(os.path.join(current_dir, '..', '..', 'src'))
-
-# Adicione o diretório src ao caminho do Python
 sys.path.append(src_dir)
 
 from services.extractor import Extractor
